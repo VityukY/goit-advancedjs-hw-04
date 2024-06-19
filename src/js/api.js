c7a7c3d2854cd45c338bbd7f2b73225abd19ff00
Yuriy Yuriy <you.thunderstruck@gmail.com>
2024-06-20 00:57:29 +0300

    fix

--- src/js/api.js
+++ src/js/api.js
@@ -1,16 +1,10 @@
 import axios from 'axios';
 
-const requestHandler = async request => {
+const requestHandler = async (request, page) => {
   const result = await axios.get(
-    `https://pixabay.com/api/?key=20439634-6c644a175487626659667f77f&q=${request}&image_type=photo&orientation=horizontal&safesearch=true`
+    `https://pixabay.com/api/?key=20439634-6c644a175487626659667f77f&q=${request}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
   );
   return result;
 };
 
 export { requestHandler };
-
-console.log(
-  requestHandler('flower').then(data => {
-    console.log(data.data.hits);
-  })
-);
--- src/main.js
+++ src/main.js
@@ -1,25 +1,116 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import { requestHandler } from './js/api';
 
+const formRef = document.querySelector('.search-form');
+const galleryRef = document.querySelector('.gallery');
+const loadBrnRef = document.querySelector('.load-more');
+
+let page = 1;
+let searchQuery = '';
+
+loadBrnRef.addEventListener('click', e => {
+  page += 1;
+  galleryHandler(searchQuery, page);
+});
+
+formRef.addEventListener('submit', e => {
+  e.preventDefault();
+  galleryRef.innerHTML = '';
+  searchQuery = formRef.searchQuery.value;
+  galleryHandler(searchQuery, page);
+});
+
+function galleryHandler(searchQuery, page) {
+  const serchData = requestHandler(searchQuery, page);
+  serchData
+    .then(result => {
+      return result.data.hits;
+    })
+    .then(data => {
+      /* 
+      let htmlString = '';
+      data.forEach(image => {
+        const currentCard = imageCartHandler(image);
+        htmlString += currentCard;
+      });
+      galleryRef.innerHTML = htmlString;
+      */
+      const galleryItems = data.map(elem => {
+        return createCard(elem);
+      });
+      galleryRef.append(...galleryItems);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
 function imageCartHandler(data) {
-  const cart = ` <div class="photo-card">
-    <img src="" alt="" loading="lazy" />
+  const cart = `<div class="photo-card">
+    <img src="${data.webformatURL}" alt="${data.tags}" loading="lazy" />
     <div class="info">
       <p class="info-item">
-        <b>Likes</b>
+        <b>${data.likes} Likes</b>
       </p>
       <p class="info-item">
-        <b>Views</b>
+        <b>${data.views} Views</b>
       </p>
       <p class="info-item">
-        <b>Comments</b>
+        <b>${data.comments}Comments</b>
       </p>
       <p class="info-item">
-        <b>Downloads</b>
+        <b>${data.downloads} Downloads</b>
       </p>
     </div>`;
   return cart;
+}
+
+function createCard(data) {
+  // Create the main card container
+  const card = document.createElement('div');
+  card.classList.add('photo-card');
+
+  // Create the image element
+  const img = document.createElement('img');
+  img.src = data.webformatURL;
+  img.alt = data.tags;
+  img.loading = 'lazy';
+
+  // Append the image to the card
+  card.appendChild(img);
+
+  // Create the info container
+  const info = document.createElement('div');
+  info.classList.add('info');
+
+  // Create the likes element
+  const likes = document.createElement('p');
+  likes.classList.add('info-item');
+  likes.innerHTML = `<b>${data.likes} Likes</b>`;
+  info.appendChild(likes);
+
+  // Create the views element
+  const views = document.createElement('p');
+  views.classList.add('info-item');
+  views.innerHTML = `<b>${data.views} Views</b>`;
+  info.appendChild(views);
+
+  // Create the comments element
+  const comments = document.createElement('p');
+  comments.classList.add('info-item');
+  comments.innerHTML = `<b>${data.comments} Comments</b>`;
+  info.appendChild(comments);
+
+  // Create the downloads element
+  const downloads = document.createElement('p');
+  downloads.classList.add('info-item');
+  downloads.innerHTML = `<b>${data.downloads} Downloads</b>`;
+  info.appendChild(downloads);
+
+  // Append the info container to the card
+  card.appendChild(info);
+
+  return card;
 }
