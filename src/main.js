ca994ea4997a99601e42388970d778a84270abef
Yuriy Yuriy <you.thunderstruck@gmail.com>
2024-06-20 17:42:07 +0300

    add simplebox and notifications

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ const loadBrnRef = document.querySelector('.load-more');
 
 let page = 1;
 let searchQuery = '';
+const lightbox = new SimpleLightbox('.gallery a', {});
 
 loadBrnRef.addEventListener('click', e => {
   page += 1;
@@ -23,53 +24,83 @@ formRef.addEventListener('submit', e => {
   galleryHandler(searchQuery, page);
 });
 
+galleryRef.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.tagName == 'IMG') {
+    lightbox.show();
+  }
+});
+
 function galleryHandler(searchQuery, page) {
   const serchData = requestHandler(searchQuery, page);
   serchData
     .then(result => {
-      return result.data.hits;
+      if (result.data.totalHits == 0) {
+        throw error;
+      }
+      if (page == 1) {
+        iziToast.success({
+          title: 'Success',
+          message: `Hooray! We found ${result.data.totalHits} images.`,
+        });
+      }
+      return result.data;
     })
     .then(data => {
-      const galleryItems = data.map(elem => {
+      const galleryItems = data.hits.map(elem => {
         return createCard(elem);
       });
       galleryRef.append(...galleryItems);
+      lightbox.refresh();
+
+      if (galleryItems.length > 0) {
+        const firstItem = galleryItems[0];
+        const headerHeight = 120;
+        const topPosition = firstItem.offsetTop - headerHeight;
+        window.scrollTo({
+          top: topPosition,
+          behavior: 'smooth',
+        });
+      }
+      if (endHitsCheker(data.totalHits, galleryRef)) {
+        loadBrnRef.classList.remove('hidden');
+      } else {
+        iziToast.warning({
+          title: 'End of list',
+          message: "We're sorry, but you've reached the end of search results.",
+        });
+        loadBrnRef.classList.add('hidden');
+      }
     })
     .catch(error => {
-      console.log(error);
+      loadBrnRef.classList.add('hidden');
+      iziToast.error({
+        title: 'No matches',
+        message: 'No matches with you request',
+      });
     });
 }
-function imageCartHandler(data) {
-  const cart = `<div class="photo-card">
-    <img src="${data.webformatURL}" alt="${data.tags}" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>${data.likes} Likes</b>
-      </p>
-      <p class="info-item">
-        <b>${data.views} Views</b>
-      </p>
-      <p class="info-item">
-        <b>${data.comments}Comments</b>
-      </p>
-      <p class="info-item">
-        <b>${data.downloads} Downloads</b>
-      </p>
-    </div>`;
-  return cart;
+
+function endHitsCheker(totalHits, galleryRef) {
+  let childNodes = galleryRef.childNodes;
+  let numberOfChildNodes = childNodes.length;
+  if (totalHits == numberOfChildNodes) {
+    return false;
+  }
+  return true;
 }
 
 function createCard(data) {
   // Create the main card container
-  const card = document.createElement('div');
+  const card = document.createElement('a');
   card.classList.add('photo-card');
+  card.href = data.largeImageURL;
 
   // Create the image element
   const img = document.createElement('img');
   img.src = data.webformatURL;
   img.alt = data.tags;
   img.loading = 'lazy';
-
   // Append the image to the card
   card.appendChild(img);
 
@@ -80,25 +111,25 @@ function createCard(data) {
   // Create the likes element
   const likes = document.createElement('p');
   likes.classList.add('info-item');
-  likes.innerHTML = `<b>${data.likes} Likes</b>`;
+  likes.innerHTML = `<b>${data.likes}<br>Likes</br>`;
   info.appendChild(likes);
 
   // Create the views element
   const views = document.createElement('p');
   views.classList.add('info-item');
-  views.innerHTML = `<b>${data.views} Views</b>`;
+  views.innerHTML = `<b>${data.views}<br>Views</b>`;
   info.appendChild(views);
 
   // Create the comments element
   const comments = document.createElement('p');
   comments.classList.add('info-item');
-  comments.innerHTML = `<b>${data.comments} Comments</b>`;
+  comments.innerHTML = `<b>${data.comments}<br>Comments</b>`;
   info.appendChild(comments);
 
   // Create the downloads element
   const downloads = document.createElement('p');
   downloads.classList.add('info-item');
-  downloads.innerHTML = `<b>${data.downloads} Downloads</b>`;
+  downloads.innerHTML = `<b>${data.downloads}<br>Downloads</b>`;
   info.appendChild(downloads);
 
   // Append the info container to the card
